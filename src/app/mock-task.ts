51b36aa5d809fa4539fb39021cff61db8f87fef7
Lucia Sanchez-Mac <lucia_sanchez_@hotmail.com>
2023-02-07 21:54:29 -0300

    Agregue mock-tareas

---
+++ src/app/mock-task.ts
@@ -0,0 +1,32 @@
+import { Task } from "./Task"
+
+
+export const TASKS: Task[] = [
+    {
+        id: 1,
+        text: "Terminar primer módulo de Ángular",
+        day: "Agosto 5 a las 12 am",
+        reminder: true
+    },
+
+    {
+        id: 2,
+        text: "Hacer las compras",
+        day: "Agosto 5 a las 7 pm",
+        reminder: true
+    },
+
+    {
+        id: 3,
+        text: "Profundizar sobre ángular",
+        day: "Agosto 6 a las 6 pm",
+        reminder: false
+    }
+
+    {
+        id: 4,
+        text: "Arrancar módulo 4",
+        day: "Agosto 7 a las 6 pm",
+        reminder: false
+    }
+]
